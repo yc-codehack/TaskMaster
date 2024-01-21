fbec286ec1796bb8d5b40aa4fd46dd3e4b81804b
Yash Chitlangia <yash.chitlangia@unthinkable.co>
2024-01-21 16:20:04 +0530

    Added support for PWA

--- task-master/next.config.mjs
+++ task-master/next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
+
+export default withPWA(nextConfig);
